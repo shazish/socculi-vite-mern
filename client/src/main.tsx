a6838bbe8b581511cae6dd66d7945556b079688c
Shaun S <shaahin@gmail.com>
2025-02-11 23:01:37 -0600

    utilize .env / .htaccess for key storage

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -32,12 +32,12 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Auth0Provider
-        domain="dev-c7t4suh18tfmv2xh.us.auth0.com"
-        clientId="U8JAIQuIMg1CdMhDCCG4Omn3CtyjKuRP"
+        domain={import.meta.env.VITE_AUTH0_DOMAIN}
+        clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
         cacheLocation="localstorage"
         authorizationParams={{
           redirect_uri: window.location.origin,
-          audience: "https://dev-c7t4suh18tfmv2xh.us.auth0.com/api/v2/",
+          audience: import.meta.env.VITE_AUTH0_AUDIENCE,
         }}
       >
         <SocculiNavbar />
